e7f1268570fd420b5a82048deab74b0b5f3d1471
Tocea2003 <cristianioan.lavu@ulbsibiu.ro>
2026-03-11 17:10:51 +0200

    Teste

--- BusSimulator/obj/Debug/net9.0/BusSimulator.AssemblyInfo.cs
+++ BusSimulator/obj/Debug/net9.0/BusSimulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusSimulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4353bf0b123e314516953bbbabccb48eef21c115")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039f11f4f3d7055899a33974b52bf931192c5873")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusSimulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusSimulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
